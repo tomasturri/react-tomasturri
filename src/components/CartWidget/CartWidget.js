afb3cf156421d43b8cbc6b5ac915f91c1d83a45a
tomasturri <tomasturri14@gmail.com>
2023-09-14 16:49:14 -0300

    1er entrega

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import cart from "./assets/compra.svg"
+
+const CartWidget =() => {
+    return (
+        <div>
+            <img src={cart} alt="cart-widget"/>
+            0
+        </div>
+
+    )
+
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import CartWidget from "../CartWidget/CartWidget"
+
+const NavBar =() => {
+    return (
+        <nav>
+            <h3>Hormiprem</h3>
+                <div>
+                    <button>tubo</button>
+                    <button>agua</button>
+                    <button>pared</button>
+                    </div>
+                    <CartWidget />
+                </nav>
+    )
+}
+
+export default NavBar
+
+
