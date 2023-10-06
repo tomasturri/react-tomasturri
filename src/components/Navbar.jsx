a0717d95b0f1fd061c4cb5778cc56b87d8c3a203
tomasturri <tomasturri14@gmail.com>
2023-10-05 23:16:03 -0300

    2da entrega

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CartWidget from './CartWidget';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <Link to="/" className="logo"><h1>Hormiprem</h1></Link>
+        <ul className="menu">
+            <li><Link className="menu-link" to="/">Inicio</Link></li>
+            <li><Link className="menu-link" to="/productos">Productos</Link></li>
+            <li><Link className="menu-link" to="/productos/construccion">Construcción</Link></li>
+            <li><Link className="menu-link" to="/productos/jardineria">Jardinería</Link></li>
+            <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
+            <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
+            <li><CartWidget /></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
