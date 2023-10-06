8bd87ac40b71795fd618eec11616d5d9068dceeb
tomasturri <tomasturri14@gmail.com>
2023-10-06 13:07:18 -0300

    correccion de errores

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,40 +1,34 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
+import data from "../data/data.json" // Ajusta la ruta según la ubicación real de data.json
+
 
 const ItemListContainer = () => {
-
     const [productos, setProductos] = useState([]);
-
     const [titulo, setTitulo] = useState("Productos");
-
     const categoria = useParams().categoria;
 
     useEffect(() => {
+        if (categoria) {
+            // Filtra los productos según la categoría seleccionada
+            const productosFiltrados = data.filter((producto) => producto.categoria === categoria);
+            setProductos(productosFiltrados);
+            setTitulo(`Productos - ${categoria}`);
+        } else {
+            // Si no se selecciona una categoría, muestra todos los productos
+            setProductos(data);
+            setTitulo("Productos");
+        }
+    }, [categoria]);
 
-      const productosRef = collection(db, "productos");
-      const q = categoria ? query(productosRef, where("categoria", "==", categoria)) : productosRef;
-
-      getDocs(q)
-        .then((resp) => {
-
-          setProductos(
-            resp.docs.map((doc) => {
-              return { ...doc.data(), id: doc.id }
-            })
-          )
-        })
-        
-    }, [categoria])
-    
-    
-  return (
-    <div>
-        <ItemList productos={productos} titulo={titulo} />
-    </div>
-  )
+    return (
+        <div>
+            <ItemList productos={productos} titulo={titulo} />
+        </div>
+    );
 }
 
-export default ItemListContainer
+export default ItemListContainer;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnrXcfm2DFzzbMwVd2ty4oydZ9qSnGiB8",
-  authDomain: "carpishop-eb6ea.firebaseapp.com",
-  projectId: "carpishop-eb6ea",
-  storageBucket: "carpishop-eb6ea.appspot.com",
-  messagingSenderId: "380430211990",
-  appId: "1:380430211990:web:ad8ce8cba96769b84bdbb7"
 };
 
 // Initialize Firebase
